8f2ffb84f803074a8cf585a082a52208c298cdaa
Sébastien Alix <sebastien.alix@camptocamp.com>
2022-01-03 11:28:01 +0100

    [IMP] shipment: bind dock to a warehouse
    And filter available docks from a shipment to the current warehouse.

--- stock_dock/models/stock_dock.py
+++ stock_dock/models/stock_dock.py
@@ -8,6 +8,26 @@ class StockDock(models.Model):
     _name = "stock.dock"
     _description = "Dock, used by trucks to load/unload goods"
 
+    def _default_warehouse_id(self):
+        wh = self.env.ref("stock.warehouse0", raise_if_not_found=False)
+        return wh.id or False
+
     name = fields.Char(required=True)
     barcode = fields.Char()
     active = fields.Boolean(string="Active", default=True)
+    warehouse_id = fields.Many2one(
+        comodel_name="stock.warehouse",
+        ondelete="cascade",
+        string="Warehouse",
+        required=True,
+        check_company=True,
+        default=_default_warehouse_id,
+    )
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Company",
+        related="warehouse_id.company_id",
+        readonly=True,
+        store=True,
+        index=True,
+    )
